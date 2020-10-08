46d8d4e136284fc8345c45dc2cd633ed7a4ea6a2
Celso Lorensatto <lorensatto99@outlook.com>
2020-10-08 14:15:13 -0300

    Adicionando o SCRIPT de Endereço,Ajustando bairros ROGÉRIO

---
+++ SCRIPT - ENDEREÇO V2.0.py
@@ -0,0 +1,198 @@
+Estados = {1: 'Acre', 2: 'Alagoas', 3: 'Amapá', 4: 'Amazonas', 5: 'Bahia', 6: 'Ceará', 7: 'Espírito Santo', 8: 'Goiás', 9: 'Maranhão',10:'Mato Grosso',11:'Mato Grosso do Sul',12:'Minas Gerais',13:'Pará',14:'Paraíba',15:'Paraná',16:'Pernambuco',17:'Piauí',18:'Rio de Janeiro',19:'Rio Grande do Norte',20:'Rio Grande Do Sul',21:'Rondônia',22:'Roraima',23:'Santa Catarina',24:'São Paulo',25:'Sergipe',26:'Tocantins'}
+cidades = {1: 'Rio Branco', 2: 'Acrelândia', 3: 'Porto Acre', 4: 'Tarauacá', 5: 'Rodrigues Alves', 6: 'Xapuri', 7: 'Capixaba', 8: 'Bujari', 9: 'Brasileia', 10: 'Assis Brasil', 11: 'Maceió', 12: 'Batalha', 13: 'Belêm', 14: 'Cajueiro', 15: 'Canapi', 16: 'Carneiros', 17: 'Caruripe', 18: 'Flexeiras', 19: 'Igaci', 20: 'Japaratinga', 21: 'Macapá', 22: 'Cutias', 23: 'Mazagão', 24: 'Oiapoque', 25: 'Santana', 26: 'Serra do Navio', 27: 'Tartarugalzinho', 28: 'Vitória do Jari', 29: 'Pracuúba', 30: 'Itaubal', 31: 'Manaus', 32: 'Coari', 33: 'Lábrea', 34: 'Maués', 35: 'Borba', 36: 'Careiro', 37: 'Tartarugalzinho', 38: 'Manacapuru', 39: 'Eirunepé', 40: 'Boca do Acre', 41: 'Salvador', 42: 'Feira de Santana', 43: 'Vitória da Conquista', 44: 'Camaçari', 45: 'Juazeiro', 46: 'Itabuna', 47: 'Lauro de Freitas', 48: 'Ilhéus', 49: 'Jequié', 50: 'Teixeira de Freitas', 51: 'Fortaleza', 52: 'Caucaia', 53: 'Juazeiro do Norte', 54: 'Maracanaú', 55: 'Sobral', 56: 'Itapipoca', 57: 'Maranguage', 58: 'Iguatu', 59: 'Quixadá', 60: 'Crato', 61: 'Vitória', 62: 'Vila Velha', 63: 'Cariacica', 64: 'Cachoeiro de Itapemirim', 65: 'Linhares', 66: 'São Mateus', 67: 'Guarapari', 68: 'Colatina', 69: 'Aracruz', 70: 'Viana', 71: 'Goiânia', 72: 'Niquelândia', 73: 'Mineiros', 74: 'Caiapônia', 75: 'Rio Verde', 76: 'Nova Crixás', 77: 'Cavalcante', 78: 'São Miguel do Araguaia', 79: 'Serranópolis', 80: 'Crixás', 81: 'São Luís', 82: 'Imperatriz', 83: 'São José de Ribamar', 84: 'Timon', 85: 'Caxias', 86: 'Codó', 87: 'Açailandia', 88: 'Paço do Lumiar', 89: 'Pinheiro', 90: 'Chapadinha', 91: 'Cuiabá', 92: 'Várzea Grande', 93: 'Rondonópolis', 94: 'Sinop', 95: 'Tangará da Serra', 96: 'Cáceres', 97: 'Barra do Garças', 98: 'Lucas do Rio Verde', 99: 'Juína', 100: 'Colzina', 101: 'Campo Grande', 102: 'Dourados', 103: 'Três Lagoas', 104: 'Corumbá', 105: 'Ponta Porã', 106: 'Sidrolândia', 107: 'Navirai', 108: 'Nova Andradina', 109: 'Aquidauana', 110: 'Maracaju', 111: 'Bole Horizonte', 112: 'Uberlândia', 113: 'Contagem', 114: 'Juiz de Fora', 115: 'Betim', 116: 'Uberaba', 117: 'Ipatinga', 118: 'Divinópolis', 119: 'Santa Luzia', 120: 'Ibirité', 121: 'Belém', 122: 'Ananindeua', 123: 'Santarém', 124: 'Marabá', 125: 'Parauapebas', 126: 'Brangaça', 127: 'São Felix do Xingu', 128: 'Barcarena', 129: 'Tucuruí', 130: 'Paragominas', 131: 'João Pessoa', 132: 'Barra de Santa Rosa', 133: 'Cabedelo', 134: 'Baveux', 135: 'Aguiar', 136: 'Alagoa Grande', 137: 'Alagoa Nova', 138: 'Brejo do Cruz', 139: 'Pilões', 140: 'Serraria', 141: 'Curitiba', 142: 'Juranda', 143: 'Moreira Sales', 144: 'Ubiratã', 145: 'Arapoti', 146: 'Tibagi', 147: 'Ventania', 148: 'Foz do Jordão', 149: 'Foz do Iguaçu', 150: 'Palmas', 151: 'Recife', 152: 'Casinhas', 153: 'Frei Miguelzinho', 154: 'Agestina', 155: 'Lagoa dos Gatos', 156: 'Panelas', 157: 'Sairé', 158: 'São Joaquim do Monte', 159: 'Calçado', 160: 'Canhotinho', 161: 'Teresina', 162: 'Domingos Mourão', 163: 'Barra Duro', 164: 'Hugo Napoleão', 165: 'Jardim do Mulato', 166: 'Beneditinos', 167: 'Altos', 168: 'José de Freitas', 169: 'Aroazes', 170: 'Brasileira', 171: 'Rio de Janeiro', 172: 'São Gonçalo', 173: 'Duque de Caxias', 174: 'Nova Iguaçu', 175: 'Niterói', 176: 'Belford Roxo', 177: 'Campos dos Goytacazes', 178: 'São João de Meriti', 179: 'Petrópolis', 180: 'Angra dos Reis', 181: 'Natal', 182: 'Mossoró', 183: 'Parnamirim', 184: 'Macaíba', 185: 'São Gonçalo do Amarante', 186: 'Caicó', 187: 'Currais Novos', 188: 'Nova Cruz', 189: 'Santa Cruz', 190: 'Apodi', 191: 'Porto Alegre', 192: 'Caxias do Sul', 193: 'Canoas', 194: 'Pelotas', 195: 'Santa Maria', 196: 'Gravataí', 197: 'Novo Hamburgo', 198: 'Passo Fundo', 199: 'Alvorada', 200: 'Erechim', 201: 'Porto Velho', 202: 'Ji-Paraná', 203: 'Ariquemes', 204: 'Vilhena', 205: 'Cacoal', 206: 'Rolim de Moura', 207: 'Jaru', 208: 'Guajará-Mirim', 209: 'Pimenta Bueno', 210: 'Buritis', 211: 'Boa Vista', 212: 'Ronainópolis', 213: 'Caracaraí', 214: 'Cantá', 215: 'Mucajaí', 216: 'Pacaraima', 217: 'Alto Alegre', 218: 'Amajari', 219: 'Bonfim', 220: 'Iracema', 221: 'Florianópolis', 222: 'Palhoça', 223: 'São José', 224: 'Alfredo Wagner', 225: 'Águas Mornas', 226: 'São Bonifácio', 227: 'Canelinha', 228: 'Canoinhas', 229: 'Major Vieira', 230: 'Papanduva', 231: 'São Paulo', 232: 'Osasco', 233: 'Praia Grande', 234: 'Santos', 235: 'Guarujá', 236: 'Campinas', 237: 'Guarulhos', 238: 'Diadema', 239: 'Mauá', 240: 'Itapevi', 241: 'Aracaju', 242: 'Areia Branca', 243: 'Macambira', 244: 'São Domingos', 245: 'Riachão do Dantas', 246: 'Malhada dos Bois', 247: 'Muribeca', 248: 'Poço Verde', 249: 'Estância', 250: 'Siriri', 251: 'Palmas', 252: 'Goianorte', 253: 'Guaraí', 254: 'Miranorte', 255: 'Monte Santo do Tocantins', 256: 'Pium', 257: 'Taguatinga', 258: 'Lizarda', 259: 'Silvanópolis', 260: 'Campos Lindos'}
+Bairros = {1: 'Ipê', 2: 'Irineu Serra', 3: 'Jardim Brasil', 4: 'Centro', 5: 'Distrito Industrial', 6: 'Vila Redenção', 7: 'Centro', 8: 'São Franciso', 9: 'Zona Rural', 10: 'Cobe', 11: 'Flores', 12: 'Novo', 13: 'Centro', 14: 'São Francisco', 15: 'Zona Rural', 16: 'Aeroporto', 17: 'Ciquia', 18: 'Estrada da Variante', 19: 'Conquista', 20: 'Centro', 21: 'Zona Rural', 22: 'Centro', 23: 'Zona rural', 24: 'São Silvestre', 25: 'Alberto Castro', 26: 'Eldorado', 27: 'José Moreira', 28: 'Centro', 29: 'Cascata', 30: 'Bela Vista', 31: 'Centro', 32: 'Farol', 33: 'Ponta Verde', 34: 'Centro', 35: 'Zona Rural', 36: 'Batalhão', 37: 'Campina', 38: 'Nazaré', 39: 'Pedreira', 40: 'Centro', 41: 'Cohab', 42: 'Zona Rural', 43: 'Centro', 44: 'Carie', 45: 'Zona Rural', 46: 'Centro', 47: 'Carneiros', 48: 'Zona Rural', 49: 'Centro', 50: 'Colônia Pindorama', 51: 'Comunidade Tércio Wanderley', 52: 'Centro', 53: 'Zona Rural', 54: 'Emilio Maia', 55: 'Centro', 56: 'Zona Rural', 57: 'São Bonifácio', 58: 'Centro', 59: 'Zona Rural', 60: 'Zona Urbana', 61: 'Central', 62: 'Jesus de Nazaré', 63: 'Julião Ramos', 64: 'Cutias', 65: 'Centro', 66: 'Sede', 67: 'Central', 68: 'Centro', 69: 'Olaria', 70: 'Centro', 71: 'Planalto', 72: 'Universidade', 73: 'Paraíso', 74: 'Centro', 75: 'Comercial', 76: 'Centro', 77: 'Vila Intermediária', 78: 'Vila Primaria', 79: 'Vila ADministrativa', 80: 'Centro', 81: 'Central', 82: 'Centro', 83: 'Prainha', 84: 'Mina', 85: 'Centro', 86: 'Central', 87: 'Sede', 88: 'Centro', 89: 'Macacoari', 90: 'Ribeirinha', 91: 'Centro', 92: 'Nossa Senhora das Graças', 93: 'Cidade Nova', 94: 'Centro', 95: 'Taua-Mirim', 96: 'Chagas Aguiar', 97: 'Centro', 98: 'Barra Limpa', 99: 'B da Fonte]', 100: 'Centro', 101: 'Ramalho r.', 102: 'Santa Luzia', 103: 'Centro', 104: 'Ipiranga', 105: 'Recreio', 106: 'Centro', 107: 'Castanho', 108: 'Sebastião Borges', 109: 'Centro', 110: 'Central', 111: 'Sede', 112: 'Centro', 113: 'Liberdade', 114: 'São José', 115: 'Centro', 116: 'Nossa Senhora Fátima', 117: 'Santo Antônio', 118: 'Centro', 119: 'Praia do Gado', 120: 'Plato Piquiá', 121: 'Pituba', 122: 'Caminho das Árvores', 123: 'Comércio', 124: 'Centro', 125: 'Serraria Brasil', 126: 'Brasília',127:'Boa Vista',128:'Ayrton Senna',129:'Bateias',130:'Abrantes'}
+inclusao = []
+
+verificacao1 = False
+verificacao2 = False
+
+j = int(input("digite o \033[32m ID \033[0m de \033[32m INICIO \033[0m do \033[32m BAIRRO \033[0m \n"))
+encerra = int(input("Agora digite o \033[32m ID \033[0m de \033[32m TERMINO \033[0m do \033[32m BAIRRO \033[0m \n"))+1
+i = int(input("digite o \033[32m ID \033[0m de \033[32m INICIO\033[0m do \033[32m ENDEREÇO \033[0m \n"))
+Bairro = j
+idBairro = 0
+idCidade = 0
+Cidade = 0
+Estado = 0
+
+if int(Bairro%3) == 0:
+    Cidade = Bairro / 3
+    idBairro = 1
+    if int(Cidade % 10) == 0:
+        Estado = Cidade / 10
+        idCidade = 1
+    elif int(Cidade % 10) >= 9:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 8:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 7:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 6:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 5:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 4:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 3:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 2:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 1:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+
+elif int(Bairro%3) >= 2:
+    Cidade = int(Bairro / 3) + 1
+    idBairro = 2
+    if int(Cidade % 10) == 0:
+        Estado = Cidade / 10
+        idCidade = 1
+    elif int(Cidade % 10) >= 9:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 8:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 7:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 6:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 5:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 4:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 3:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 2:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 1:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+
+elif int(Bairro%3) >= 1:
+    Cidade = int(Bairro / 3) + 1
+    idBairro = 3
+    if int(Cidade % 10) == 0:
+        Estado = Cidade / 10
+        idCidade = 1
+    elif int(Cidade % 10) >= 9:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 8:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 7:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 6:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 5:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 4:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 3:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+    elif int(Cidade % 10) >= 2:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 3
+    elif int(Cidade % 10) >= 1:
+        Estado = int(Cidade / 10) + 1
+        idCidade = 2
+
+lancamentos = (encerra-j)*5
+contador = 0
+
+#j = numero da cidade
+while j != encerra:
+    #i = numero do bairro
+    for x in range(5):
+        nomebairro = input("Insira o endereço \033[33m{}\033[0m do bairro \033[34m{}\033[0m da Cidade de \033[34m{}\033[0m do Estado : \033[34m{}\033[0m \n".format(x+1,Bairros[Bairro],cidades[Cidade],Estados[Estado]))
+        nCep = input("Insira o \033[32m NUMERO DE CEP SEM O TRAÇO\033[0m do endereço \033[33m{}\033[0m do bairro \033[34m{}\033[0m da Cidade de \033[34m{}\033[0m do Estado : \033[34m{}\033[0m \n".format(x+1,Bairros[Bairro],cidades[Cidade],Estados[Estado]))
+        while len(nCep) != 8:
+            print("\033[31mINSIRA O NUMERO DO CEP VÁLIDO\033[0m")
+            nCep = input("Insira o \033[32m NUMERO DE CEP SEM O TRAÇO com 8 Dígitos\033[0m\n")
+        sentenca = """"INSERT INTO T_SAK_ENDERECO (cd_end_correio,cd_bairro,nr_cep,ds_logradouro) VALUES ({},{},{},'{}');""".format(i, j,nCep,nomebairro)
+        inclusao.append(sentenca)
+        i += 1
+        contador += 1
+    j +=1
+    Bairro +=1
+    idBairro-=1
+    if idBairro == 0:
+        idBairro = 3
+        Cidade += 1
+        idCidade -=1
+        if idCidade == 0:
+            idCidade = 10
+            Estado += 1
+
+    if contador > lancamentos/3 and verificacao1 == False:
+        print("\033[31mVOCÊ JA FEZ {} LANÇAMENTOS DE  {} QUE FALTAM PARA VOCÊ FINALIZAR\033[0m".format(contador,lancamentos))
+        resposta = input("\033[32m VOCÊ JA ESTA 1/3 DO CAMINHO GOSTARIA DE FAZER UMA PAUSA ?\033[0m\n S para sim ou N para não\n").upper()
+        if resposta == "S":
+            for elemento in inclusao:
+                print(elemento)
+            j = encerra
+            break
+        if resposta == "N":
+            verificacao1 = True
+            continue
+        else:
+            while resposta != "S" or resposta != "N":
+                resposta = input("\033[32m NÃO ENTENDI \033[0m\n \033[32mS\033[0m para sim ou \033[32mN\033[0m para não\n").upper()
+                if resposta == "S":
+                    for elemento in inclusao:
+                        print(elemento)
+                    j = encerra
+                    break
+                if resposta == "N":
+                    verificacao1 = True
+
+
+    if contador > lancamentos/2 and verificacao2 == False:
+        print("\033[31mVOCÊ JA FEZ {} LANÇAMENTOS DE  {} QUE FALTAM PARA VOCÊ FINALIZAR\033[0m".format(contador,lancamentos))
+        resposta = input("\033[32m VOCÊ JA ESTA NA METADE DO CAMINHO GOSTARIA DE FAZER UMA PAUSA ?\033[0m\n S para sim ou N para não").upper()
+        if resposta == "S":
+            for elemento in inclusao:
+                print(elemento)
+            j = encerra
+            break
+        if resposta == "N":
+            verificacao2 = True
+        else:
+                while resposta != "S" or resposta != "N":
+                    resposta = input(
+                        "\033[32m NÃO ENTENDI \033[0m\n \033[32mS\033[0m para sim ou \033[32mN\033[0m para não").upper()
+                    if resposta == "S":
+                        for elemento in inclusao:
+                            print(elemento)
+                        j = encerra
+                        break
+                    if resposta == "N":
+                        verificacao1 = True
+
+
+
+if contador == lancamentos:
+    print("\033[32m PARABÉNS PARÇA VC TERMINOU O ENDEREÇO POURRA !!! \033[0m")
+    for elemento in inclusao:
+        print(elemento)
+
