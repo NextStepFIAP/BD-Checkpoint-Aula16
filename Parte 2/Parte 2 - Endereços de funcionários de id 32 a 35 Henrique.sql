4c112cb5bcfeb0a4535bb48965dbb18877ef12b3
HenriqueCoto <61854437+HenriqueCoto@users.noreply.github.com>
2020-10-12 00:31:26 -0300

    Create Parte 2 - Endereços de funcionários de id 32 a 35 Henrique.sql

---
+++ Parte 2/Parte 2 - Endereços de funcionários de id 32 a 35 Henrique.sql
@@ -0,0 +1,8 @@
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (32,64,3458,1,'NULL',508);															
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (32,65,3459,1,'NULL',677);															
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (33,66,3460,1,'NULL',56);														
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (33,67,3461,1,'NULL',340);															
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (34,68,3462,1,'NULL',843);															
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (34,69,3463,1,'NULL',700);															
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (35,70,3464,1,'NULL',45);															
+INSERT INTO T_SAK_FUNCIONARIO_ENDERECO (cd_func,cd_end_func,cd_end_correio,cd_tipo_endereco,ds_complemento,nr_logradouro) VALUES (35,71,3465,1,'NULL',890);															
